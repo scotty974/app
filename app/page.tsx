cd5408bf25dc8029b2cee0c28eed9ce1b9e6c2f5
scotty974 <fabienethevepro@gmail.com>
2024-02-11 12:53:03 +0100

    delete /

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   useEffect(() => {
     const fetchdata = async () => {
       try {
-        const resp = await fetch("/api/words/getAll", {
+        const resp = await fetch("api/words/getAll", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
